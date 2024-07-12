5256416cc8b3d7c4d780ac2c37ff54f935c098b3
Fozle Rabbi <fozlerabbi9790@gmail.com>
2024-07-12 12:59:37 +0600

    code build

--- dist/app.js
+++ dist/app.js
@@ -5,12 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const routes_1 = __importDefault(require("./app/routes"));
 const app = (0, express_1.default)();
 //parser
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
-app.get("/", (req, res) => {
-    const a = 10;
-    res.send(a);
+app.use('/api/v1', routes_1.default);
+app.get('/', (req, res) => {
+    res.send('server running successfully');
 });
 exports.default = app;
